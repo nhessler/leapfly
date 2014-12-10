81ef9373a10443bfe4cedd87e623f0eeedb7ca9d
Nathan Hessler <xmilestegx@gmail.com>
2014-12-10 10:21:07 -0500

    initial code for testing crazyflie (works!)

---
+++ leapfly.rb
@@ -0,0 +1,11 @@
+require 'artoo'
+
+connection :crazyflie, :adaptor => :crazyflie
+device :drone, :driver => :crazyflie, :connection => :crazyflie, :interval => 0.1
+
+work do
+  drone.start
+  drone.take_off
+
+  after(1.seconds) {drone.stop}
+end
